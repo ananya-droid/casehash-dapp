f10c5f7b7a28f02ca052783015c25d6530d241bf
ananya <ananyakumariwork@gmail.com>
2025-09-16 22:42:39 +0530

    feat: Complete Judicial Document Verification System with smart contracts and React frontend

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+const { ALCHEMY_API_KEY, PRIVATE_KEY } = process.env;
 
-/** @type import('hardhat/config').HardhatUserConfig */
+
+
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
